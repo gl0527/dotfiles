3bb272641139d3c33523ebe43eb35c9d007b3d53
Lajos.Gurzo <lajos.gurzo@cargometer.com>
2021-05-24 17:12:11 +0200

    Unhide config files

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-ln -sf $SCRIPT_DIR/.bashrc $HOME/.bashrc
-ln -sf $SCRIPT_DIR/.gitconfig $HOME/.gitconfig
-ln -sf $SCRIPT_DIR/.vimrc $HOME/.nvimrc
-ln -sf $SCRIPT_DIR/.vimrc $HOME/.vimrc
-ln -sf $SCRIPT_DIR/.tmux.conf $HOME/.tmux.conf
-ln -sf $SCRIPT_DIR/.gdbinit $HOME/.gdbinit
+ln -sf $SCRIPT_DIR/bashrc $HOME/.bashrc
+ln -sf $SCRIPT_DIR/gitconfig $HOME/.gitconfig
+ln -sf $SCRIPT_DIR/vimrc $HOME/.nvimrc
+ln -sf $SCRIPT_DIR/vimrc $HOME/.vimrc
+ln -sf $SCRIPT_DIR/tmux.conf $HOME/.tmux.conf
+ln -sf $SCRIPT_DIR/gdbinit $HOME/.gdbinit
