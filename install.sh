8793e396b50a02aafc341c843efa776869d4ecc8
gl0527 <gurzolajos27@gmail.com>
2021-05-24 17:57:16 +0200

    nit - Make install script verbose

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-ln -sf $SCRIPT_DIR/bashrc $HOME/.bashrc
-ln -sf $SCRIPT_DIR/gitconfig $HOME/.gitconfig
-ln -sf $SCRIPT_DIR/vimrc $HOME/.nvimrc
-ln -sf $SCRIPT_DIR/vimrc $HOME/.vimrc
-ln -sf $SCRIPT_DIR/tmux.conf $HOME/.tmux.conf
-ln -sf $SCRIPT_DIR/gdbinit $HOME/.gdbinit
+ln -sfv $SCRIPT_DIR/bashrc $HOME/.bashrc
+ln -sfv $SCRIPT_DIR/gitconfig $HOME/.gitconfig
+ln -sfv $SCRIPT_DIR/vimrc $HOME/.nvimrc
+ln -sfv $SCRIPT_DIR/vimrc $HOME/.vimrc
+ln -sfv $SCRIPT_DIR/tmux.conf $HOME/.tmux.conf
+ln -sfv $SCRIPT_DIR/gdbinit $HOME/.gdbinit
