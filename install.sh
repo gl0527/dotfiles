d687ca10378adc90c9ae365fda2b2903445521c9
gl0527 <gurzolajos27@gmail.com>
2021-06-04 23:38:16 +0200

    Set dotfiles only if their corresponding program exists

--- install.sh
+++ install.sh
@@ -3,10 +3,47 @@
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 ln -sfv $SCRIPT_DIR/bashrc $HOME/.bashrc
-ln -sfv $SCRIPT_DIR/gitconfig $HOME/.gitconfig
-ln -sfv $SCRIPT_DIR/tmux.conf $HOME/.tmux.conf
-ln -sfv $SCRIPT_DIR/gdbinit $HOME/.gdbinit
-ln -sfv $SCRIPT_DIR/vim/vimrc $HOME/.vimrc
-ln -sfv $SCRIPT_DIR/vim/vimrc $HOME/.config/nvim/init.vim
-ln -sfv $SCRIPT_DIR/vim/ftplugin/cpp.vim $HOME/.vim/ftplugin/cpp.vim
-ln -sfv $SCRIPT_DIR/vim/ftplugin/cpp.vim $HOME/.config/nvim/ftplugin/cpp.vim
+
+# Install gdb config
+if command -v gdb &> /dev/null
+then
+    ln -sfv $SCRIPT_DIR/gdbinit $HOME/.gdbinit
+else
+    echo "gdb not found"
+fi
+
+# Install git config
+if command -v git &> /dev/null
+then
+    ln -sfv $SCRIPT_DIR/gitconfig $HOME/.gitconfig
+else
+    echo "git not found"
+fi
+
+# Install tmux config
+if command -v tmux &> /dev/null
+then
+    ln -sfv $SCRIPT_DIR/tmux.conf $HOME/.tmux.conf
+else
+    echo "tmux not found"
+fi
+
+# Install vim config
+if command -v vim &> /dev/null
+then
+    mkdir -p $HOME/.vim/ftplugin/
+    ln -sfv $SCRIPT_DIR/vim/vimrc $HOME/.vimrc
+    ln -sfv $SCRIPT_DIR/vim/ftplugin/cpp.vim $HOME/.vim/ftplugin/cpp.vim
+else
+    echo "vim not found"
+fi
+
+# Install nvim config
+if command -v nvim &> /dev/null
+then
+    mkdir -p $HOME/.config/nvim/ftplugin/
+    ln -sfv $SCRIPT_DIR/vim/vimrc $HOME/.config/nvim/init.vim
+    ln -sfv $SCRIPT_DIR/vim/ftplugin/cpp.vim $HOME/.config/nvim/ftplugin/cpp.vim
+else
+    echo "nvim not found"
+fi
