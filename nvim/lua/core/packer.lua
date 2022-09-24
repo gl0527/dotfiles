80e1895c00ed38e14936833c11b02008a10da931
gl0527 <gurzolajos27@gmail.com>
2022-09-24 17:30:25 +0200

    Modify packer file

--- nvim/lua/core/packer.lua
+++ nvim/lua/core/packer.lua
@@ -4,25 +4,33 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'                                      -- Package manager.
-  use 'folke/tokyonight.nvim'                                       -- Fancy color scheme.
-  use 'neovim/nvim-lspconfig'                                       -- Configurations for nvim LSP.
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }   -- Fuzzy finder.
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }                                                                 -- Highly extendable fuzzy finder.
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }                                                                 -- Statusline written in lua.
-  use 'hrsh7th/cmp-nvim-lsp'                                        -- A completion engine written in lua.
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
-  use 'SirVer/ultisnips'                                            -- Snippet engine.
-  use 'quangnguyen30192/cmp-nvim-ultisnips'
-  use 'preservim/nerdcommenter'                                     -- Commenter.
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'                                      -- Package manager.
+    use 'folke/tokyonight.nvim'                                       -- Fancy color scheme.
+    use 'neovim/nvim-lspconfig'                                       -- Configurations for nvim LSP.
+    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }   -- Fuzzy finder.
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }                                                                 -- Highly extendable fuzzy finder.
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }                                                                 -- Statusline written in lua.
+    use 'hrsh7th/cmp-nvim-lsp'                                        -- A completion engine written in lua.
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'hrsh7th/nvim-cmp'
+    use 'SirVer/ultisnips'                                            -- Snippet engine.
+    use 'quangnguyen30192/cmp-nvim-ultisnips'
+    use 'preservim/nerdcommenter'                                     -- Commenter.
+    use {
+        'rcarriga/nvim-dap-ui',
+        requires = {'mfussenegger/nvim-dap'}
+    }                                                                 -- Debug Adapter Protocol implementation in lua.
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    }                                                                 -- Parser for syntax highlighting.
 end)
