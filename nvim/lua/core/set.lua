21b2e63976823fd79ba56996c504abd1dd39fc3c
gl0527 <gurzolajos27@gmail.com>
2022-09-25 19:56:21 +0200

    Try to make nvim run faster

--- nvim/lua/core/set.lua
+++ nvim/lua/core/set.lua
@@ -1,7 +1,6 @@
 vim.opt.guicursor = ""
 
 vim.opt.nu = true
-vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -13,5 +12,10 @@ vim.opt.incsearch = true
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.lazyredraw = true
+vim.opt.synmaxcol = 180
+vim.opt.scrolljump = 5
 
 vim.g.mapleader = " "
